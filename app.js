4b90be26e03a0ad965f70520621941caa511686e
Igor <ais_93@mail.ru>
2018-07-07 18:07:57 +0300

    069 Setting up the First Event Listeners

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 // модуль для структуры данных
-var budgetController = (function() {
-
-})();
+var budgetController = (function() {})();
 
 // модуль UI
-var UIController = (function() {
+var UIController = (function() {})();
 
-})();
-
-// модуль контроллера
+// модуль контроллера приложения
 var controller = (function(budgetCtrl, UICtrl) {
+  // добавление нового элемента
+  var ctrlAddItem = function() {};
+  // обработчик нажатия на кнопку
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
 
+  // обработчик нажатия на Enter
+  document.addEventListener("keypress", function(event) {
+    //   which для старых браузеров
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 })(budgetController, UIController);
