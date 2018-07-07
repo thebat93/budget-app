2e2f1db97648f1d9850ff3461a6f9293b9f7ac35
Igor <ais_93@mail.ru>
2018-07-07 15:04:17 +0300

    068 Implementing the Module Pattern

---
+++ app.js
@@ -0,0 +1,14 @@
+// модуль для структуры данных
+var budgetController = (function() {
+
+})();
+
+// модуль UI
+var UIController = (function() {
+
+})();
+
+// модуль контроллера
+var controller = (function(budgetCtrl, UICtrl) {
+
+})(budgetController, UIController);
