6b82b0692869a2ac34d0b6c933ce63ba5f1a4e4b
Igor <ais_93@mail.ru>
2018-07-07 18:30:34 +0300

    070 Reading Input Data

--- app.js
+++ app.js
@@ -2,14 +2,43 @@
 var budgetController = (function() {})();
 
 // модуль UI
-var UIController = (function() {})();
+var UIController = (function() {
+    // селекторы
+  var DOMStrings = {
+    inputType: ".add__type",
+    inputDecription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
+  // UI Controller API
+  return {
+    // получение значения
+    getInput: function() {
+      return {
+      // тип: доход/расход
+      type: document.querySelector(DOMStrings.inputType).value, // 'inc' / 'exp'
+      // описание
+      description = document.querySelector(DOMStrings.inputDecription).value,
+      // сумма
+      value = document.querySelector(DOMStrings.inputValue).value
+      };
+    },
+    // получение селекторов
+    getDOMStrings: function() {
+        return DOMStrings;
+    }
+  };
+})();
 
 // модуль контроллера приложения
 var controller = (function(budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMStrings();
   // добавление нового элемента
-  var ctrlAddItem = function() {};
+  var ctrlAddItem = function() {
+    var input = UICtrl.getInput(DOM.inputBtn);
+  };
   // обработчик нажатия на кнопку
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.querySelector().addEventListener("click", ctrlAddItem);
 
   // обработчик нажатия на Enter
   document.addEventListener("keypress", function(event) {
