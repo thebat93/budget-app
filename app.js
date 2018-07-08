9d13dd889a3ec38b774d34489c9af645f29e4dbd
Igor <ais_93@mail.ru>
2018-07-08 13:03:45 +0300

    072 Creating Income and Expense Function Constructors

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 // модуль для структуры данных
-var budgetController = (function() {})();
+var budgetController = (function() {
+  
+  // конструктор для расходов
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  // конструктор для доходов
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  // структура данных
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+})();
 
 // модуль UI
 var UIController = (function() {
+
   // селекторы
   var DOMStrings = {
     inputType: ".add__type",
@@ -10,15 +38,18 @@ var UIController = (function() {
     inputValue: ".add__value",
     inputBtn: ".add__btn"
   };
+
   // UI Controller API
   return {
-    // получение значения
+    // получение значений
     getInput: function() {
       return {
       // тип: доход/расход
       type: document.querySelector(DOMStrings.inputType).value, // 'inc' / 'exp'
+
       // описание
       description = document.querySelector(DOMStrings.inputDecription).value,
+
       // сумма
       value = document.querySelector(DOMStrings.inputValue).value
       };
@@ -32,15 +63,16 @@ var UIController = (function() {
 
 // модуль контроллера приложения
 var controller = (function(budgetCtrl, UICtrl) {
+
   var setupEventListeners = function() {
     var DOM = UICtrl.getDOMStrings();
-    
+
     // обработчик нажатия на кнопку
     document.querySelector().addEventListener("click", ctrlAddItem);
 
     // обработчик нажатия на Enter
     document.addEventListener("keypress", function(event) {
-      //   which для старых браузеров
+      // which для старых браузеров
       if (event.keyCode === 13 || event.which === 13) {
         ctrlAddItem();
       }
@@ -52,6 +84,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     var input = UICtrl.getInput();
   };
 
+  // Controller API
   return {
     // функция инициализации
     init: function() {
